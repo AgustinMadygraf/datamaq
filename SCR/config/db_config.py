37e02bf82282fa8a0c49d0af8eafb841e1423ab4
AgustinMadygraf <agustin.mtto.madygraf>
2024-01-17 01:19:18 -0300

    Update database connection handling and remove unused code. (generated by blackbox.ai )

--- SCR/config/db_config.py
+++ SCR/config/db_config.py
@@ -1,12 +1,11 @@
 # config/db_config.py
 import os
 
-# Mejora: separar la configuración de la base de datos en una función para mayor flexibilidad y control
 def get_db_config():
     return {
         'host': os.getenv('DB_HOST', 'localhost'),
         'user': os.getenv('DB_USER', 'root'),
-        'password': os.getenv('DB_PASSWORD', ''),
+        'password': os.getenv('DB_PASSWORD', '12345678'),
         'db': 'novus',
         'port': 3306  
     }
--- SCR/utils.py
+++ SCR/utils.py
@@ -1,10 +1,9 @@
 # utils.py
-
 import serial.tools.list_ports
-import pymysql
 import os
 import functools
 import pymysql
+from config.db_config import get_db_config  
 
 def detect_serial_ports(device_description):
     """
@@ -47,6 +46,10 @@ def clear_screen():
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
+
+
+
 def reconnect_on_failure(func):
     @functools.wraps(func)
     def wrapper_reconnect(*args, **kwargs):
@@ -54,10 +57,10 @@ def reconnect_on_failure(func):
             return func(*args, **kwargs)
         except (pymysql.OperationalError, pymysql.MySQLError) as e:
             print(f"Se detectó un error en la conexión a la base de datos: {e}. Intentando reconectar...")
-            # Intenta reconectar aquí. Puedes especificar un número de reintentos si es necesario.
             try:
+                db_config = get_db_config()  # Obtener la configuración actualizada de la base de datos
                 connection = pymysql.connect(**db_config)
-                args = (connection,) + args[1:]  # Reemplazar la conexión antigua con la nueva
+                args = (connection,) + args[1:]
                 return func(*args, **kwargs)
             except Exception as e:
                 print(f"No se pudo reconectar a la base de datos: {e}")
@@ -66,5 +69,6 @@ def reconnect_on_failure(func):
 
 @reconnect_on_failure
 def check_db_connection():
+    db_config = get_db_config()  # Obtener la configuración de la base de datos
     connection = pymysql.connect(**db_config)
     return connection
