cfc8dc1b45b303903e064544d8abd8b85706e0ad
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-03-07 15:59:24 -0300

    Update button labels in the `botonera.php` file. (generated by blackbox.ai )

--- includes/botonera.php
+++ includes/botonera.php
@@ -2,29 +2,29 @@
 <div class="botonera">
     
     <form action="<?= $_SERVER["PHP_SELF"] . '?periodo=' . $periodo . '&conta=' . ($conta - 1000*$ls_periodos[$periodo]) ?>" method="post" class="botonI">
-        <input type="submit" value="<?= $periodo . '_anterior' ?>" class="presione">
+        <input type="submit" value="<?= $periodo . ' anterior' ?>" class="presione">
     </form>
 
     <div class='spacer'></div>
 
     <form action="<?= $_SERVER["PHP_SELF"] . '?periodo=mes&conta=' . $conta ?>" method="post" class="periodo">
-        <input type="submit" value="mes" class="<?= $ref_class[$class[0]] ?>">
+        <input type="submit" value="semana" class="<?= $ref_class[$class[0]] ?>">
     </form>
 
     
 
     <form action="<?= $_SERVER["PHP_SELF"] . '?periodo=semana&conta=' . $conta ?>" method="post" class="periodo">
-        <input type="submit" value="semana" class="<?= $ref_class[$class[1]] ?>">
+        <input type="submit" value="turno" class="<?= $ref_class[$class[1]] ?>">
     </form>
 
     <form action="<?= $_SERVER["PHP_SELF"] . '?periodo=turno&conta=' . $conta ?>" method="post" class="periodo">
-        <input type="submit" value="turno" class="<?= $ref_class[$class[2]] ?>">
+        <input type="submit" value="hora" class="<?= $ref_class[$class[2]] ?>">
     </form>
 
     <div class='spacer'></div>
 
     <form action="<?= $_SERVER["PHP_SELF"] . '?periodo=' . $periodo . '&conta=' . ($conta + 1000*$ls_periodos[$periodo]) ?>" method="post" class="botonD">
-        <input type="submit" value="<?= $periodo . '_siguiente' ?>" class="presione">
+        <input type="submit" value="<?= $periodo . ' siguiente' ?>" class="presione">
     </form>
 
     <form action="<?= $_SERVER["PHP_SELF"] . '?periodo=' . $periodo ?>" method="post" class="fin">
