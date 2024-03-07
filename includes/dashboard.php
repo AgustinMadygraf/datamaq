43ba4a47765f6297af0eabfe5a9c8f4089d380cb
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-03-07 16:05:26 -0300

    Change default period to 'week' and update period options (generated by blackbox.ai )

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -8,10 +8,10 @@ $segundos = 60; // Refrescar cada 60 segundos
 
 // Variable que registra qué período de tiempo mostrar por defecto
 $periodo = 'semana';
-$ls_periodos = ['mes' => 604800, 'semana' => 28800, 'turno' => 7200];
-$ls_class = ['mes' => [1, 0, 0], 'semana' => [0, 1, 0], 'turno' => [0, 0, 1]];
+$ls_periodos = ['semana' => 604800, 'turno' => 28800, 'hora' => 7200];
+$ls_class = ['semana' => [1, 0, 0], 'turno' => [0, 1, 0], 'hora' => [0, 0, 1]];
 $ref_class = ['presione', 'presado'];
-$menos_periodo = ['mes' => 'semana', 'semana' => 'turno', 'turno' => 'turno'];
+$menos_periodo = ['semana' => 'turno', 'turno' => 'hora', 'hora' => 'hora'];
 
 // Comprobar si se cambió el período a través de GET
 if ($_GET && array_key_exists("periodo", $_GET)) {
