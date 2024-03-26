50e1cc729fd3d8387a840d332419662a2b16a147
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-03-26 17:25:34 -0300

    Refactor database functions

--- includes/db_functions.php
+++ includes/db_functions.php
@@ -3,35 +3,47 @@
 require "conn_novus.php";
 
 // Conectar a la base de datos
+/**
+ * Conectar a la base de datos.
+ *
+ * @return mysqli $conexion Objeto de conexión a la base de datos.
+ */
 function conectarBD() {
-    require 'conn.php';
-    $BD = "novus";
-    $conexion = mysqli_connect($server, $usuario, $pass, $BD);
+    $conexion = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     if (!$conexion) {
-        die('Error en la conexión de la base de datos: ' . mysqli_connect_error());
+        die("Error en la conexión de la base de datos: " . mysqli_connect_error());
     }
     return $conexion;
 }
 
-// Desconectar la conexión a la base de datos
+/**
+ * Desconectar la conexión a la base de datos.
+ *
+ * @param mysqli $conexion Objeto de conexión a la base de datos.
+ */
 function desconectarBD($conexion) {
     if (!mysqli_close($conexion)) {
-        die('Error al desconectar la base de datos');
+        die("Error al desconectar la base de datos.");
     }
 }
 
-// Obtener un array multidimensional con el resultado de la consulta
+/**
+ * Obtener un array multidimensional con el resultado de la consulta SQL.
+ *
+ * @param string $sql La consulta SQL para ejecutar.
+ * @return array $rawdata Array asociativo con los resultados de la consulta.
+ */
 function getArraySQL($sql) {
     $conexion = conectarBD();
-    if (!$result = mysqli_query($conexion, $sql)) {
-        die('Error en la consulta SQL: ' . mysqli_error($conexion));
-    }
+    $result = mysqli_query($conexion, $sql);
 
-    $rawdata = array();
-    while ($row = mysqli_fetch_array($result)) {
+    if (!$result) {
+        die("Error en la consulta SQL: " . mysqli_error($conexion));
+    }
+    $rawdata = [];
+    while ($row = mysqli_fetch_assoc($result)) {
         $rawdata[] = $row;
     }
-
     desconectarBD($conexion);
     return $rawdata;
 }
