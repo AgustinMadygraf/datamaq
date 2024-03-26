22da923235b0b2ca82f952ff4c2a40f078ecbc05
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-03-26 18:09:18 -0300

    Refactor info_display.php and remove unnecessary code

--- includes/info_display.php
+++ includes/info_display.php
@@ -2,8 +2,18 @@
 // Realizar el cálculo fuera de la estructura HTML para limpiar la presentación.
 $vel_ult_calculada = round($vel_ult / 5, 1);
 $estiloFondo = "background: linear-gradient(195deg, rgb(107,170,34) {$d[3]}%, rgb(255,164,1) {$d[2]}%, rgb(234,53,34) {$d[1]}%, rgb(100,10,5) {$d[0]}%);";
-require_once 'includes/conn_stock.php';
-require_once 'includes/db_functions.php';
+
+//$sql = "SELECT * FROM `produccion_bolsas_aux`";
+//conexcion a base de datos "registro_stock"
+//require_once 'includes/conn_stock.php';
+//$conexion = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+//$result = mysqli_query($conexion, $sql);
+//$datos = mysqli_fetch_assoc($result);
+//$datos = json_encode($datos);
+//mysqli_close($conexion);
+
+//print_r($datos);
+
 $formato = "260 x 120 x  360";
 $ancho_bobina = "790 mm";
 ?>
